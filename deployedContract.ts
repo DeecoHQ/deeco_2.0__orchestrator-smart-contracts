bbfd6af4948114607ec12e5544eb1a9d01b053d4
Okpainmo <okpainmoandrew@gmail.com>
2025-09-28 13:29:22 +0100

    chore: ...contracts deployed(onchain) information

---
+++ deployedContract.ts
@@ -0,0 +1,10 @@
+export const deployedContracts = [
+    {
+        contractName: "Ochestrator",
+        Address: "0xB0027cE6572B21Eb3A4F89B04CFCB4d98A85C060",
+    },
+    {
+        contractName: "Ochestrator Admin",
+        Address: "0x16393d0CF0D8e696AeaD2Eb4F9aa0d8210dcEb47",
+    },
+];
